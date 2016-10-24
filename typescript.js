36c46f5d2c33ff2b5651c8baaffe28e04a663721
Freddyfish <Pepsai@live.dk>
2016-10-24 14:07:32 +0200

    upload of typescript and index

--- typescript.js
+++ typescript.js
@@ -1,6 +1,25 @@
+// TYPESCRIPT.JS \\
+
+// VARIABLES
 var pet1 = "dog"
 var pet2 = "cat"
-var pet3 = "worm"
+var pet3 = "daniel"
+var playername = prompt("What is your name?", "Player");
+var playerclass = prompt("What is your favorite class?", "Rogue, Mage, Warrior");
 
+// CONFIRM THAT YOU ARE READY TO START
+confirm("Are you ready to start?");
 
-console.log (pet1 + pet2 + pet3);
+// ASK PLAYER WHAT THIER NAME IS
+function playername () {
+    if (playername !== null) {
+        console.log("Hello" + playername + "! How are you today?")
+    }
+}
+
+// ASK PLAYER WHAT CLASS THEY WOULD LIKE
+function playerclass(){
+    if (playerclass !== Mage) {
+        console.log("You picked Mage" + playername + "!")
+    }
+}
