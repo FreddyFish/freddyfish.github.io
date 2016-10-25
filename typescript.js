dfefc8e13d452b55732adae9e8700924ca56f379
Freddyfish <Pepsai@live.dk>
2016-10-25 12:09:01 +0200

    Update with navbar etc.

--- typescript.js
+++ typescript.js
@@ -1,23 +1,23 @@
 // TYPESCRIPT.JS \\
 
-// VARIABLES
+// VARIABLES.
 var pet1 = "dog"
 var pet2 = "cat"
 var pet3 = "daniel"
 var playername = prompt("What is your name?", "Player");
 var playerclass = prompt("What is your favorite class?", "Rogue, Mage, Warrior");
 
-// CONFIRM THAT YOU ARE READY TO START
+// CONFIRM THAT YOU ARE READY TO START.
 confirm("Are you ready to start?");
 
-// ASK PLAYER WHAT THIER NAME IS
+// ASK PLAYER WHAT THIER NAME IS.
 function playername () {
     if (playername !== null) {
         console.log("Hello" + playername + "! How are you today?")
     }
 }
 
-// ASK PLAYER WHAT CLASS THEY WOULD LIKE
+// ASK PLAYER WHAT CLASS THEY WOULD LIKE.
 function playerclass(){
     if (playerclass !== Mage) {
         console.log("You picked Mage" + playername + "!")
